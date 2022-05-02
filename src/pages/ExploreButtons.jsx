3141c500407584eb1b9024e6917fb0fd13f27f86
EliasGalindo0 <eliasgalindo@gmail.com>
2022-05-02 16:53:18 -0300

    Requisitos 67, 68 e 69 feitos

---
+++ src/pages/ExploreButtons.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+function ExploreButtons() {
+  const history = useHistory();
+  return (
+    <>
+      <button
+        type="button"
+        data-testid="explore-foods"
+        onClick={ () => {
+          history.push('/explore/foods');
+        } }
+      >
+        Explore Foods
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => {
+          history.push('/explore/drinks');
+        } }
+      >
+        Explore Drinks
+      </button>
+    </>
+  );
+}
+
+export default ExploreButtons;
