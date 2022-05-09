dae7e74f3dcf95b803b790f8d23e15c87d9270f1
Igormcf <igormaciel82@gmail.com>
2022-05-09 16:43:21 -0300

    Add favorite

---
+++ src/components/ShowFavoriteRecipes.jsx
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import ShareIcon from '../images/shareIcon.svg';
+import WhiteHeartIcon from '../images/whiteHeartIcon.svg';
+import BlackHeartIcon from '../images/blackHeartIcon.svg';
+import '../CSS/DoneRecipes.css';
+import { btnFavLocal, getLocalFav,
+  deleteLocalFav } from '../Helpers';
+
+function ShowFavoriteRecipes() {
+  const [copied, setCopied] = useState(false);
+  const [favStatus, setFavStatus] = useState(true);
+  const [all, setAll] = useState();
+
+  useEffect(() => {
+    const local = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+    console.log(local[0].id);
+
+    setAll(local);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const favButton = ({ target }) => {
+    console.log(target.value);
+    setFavStatus(!favStatus);
+    if (!favStatus) {
+      getLocalFav(list);
+      console.log('mandou pro local');
+    } else {
+      deleteLocalFav(id[2]);
+      console.log('apagar do local');
+    }
+  };
+
+  const renderLocalDone = () => {
+    const copyFunc = (param) => {
+      navigator.clipboard.writeText(param);
+      setCopied(true);
+    };
+
+    return all.map((item, index) => {
+      const renderTags = () => {
+        if (item.tags) {
+          let { tags } = item;
+          tags += '';
+          const eachTag = tags.split(',');
+
+          return (
+            eachTag.map((a) => (
+              <h4
+                data-testid={ `${index}-${a}-horizontal-tag` }
+                key={ a }
+              >
+                { a }
+              </h4>
+            ))
+          );
+        }
+      };
+
+      return (
+        <div key={ item.name } className="done">
+          <Link to={ `/${item.type}s/${item.id}` }>
+            <img
+              className="imgFood"
+              src={ item.image }
+              alt={ item.name }
+              data-testid={ `${index}-horizontal-image` }
+            />
+          </Link>
+
+          <div>
+            <h4
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              { `${item.nationality} - ${item.category}` }
+            </h4>
+
+            { item.alcoholicOrNot ? (
+              <h5
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                { item.alcoholicOrNot }
+              </h5>
+            ) : undefined }
+
+            <Link to={ `/${item.type}s/${item.id}` }>
+              <h4
+                data-testid={ `${index}-horizontal-name` }
+              >
+                { item.name }
+              </h4>
+            </Link>
+
+            <h4
+              data-testid={ `${index}-horizontal-done-date` }
+            >
+              { `Done in: ${item.doneDate}` }
+            </h4>
+
+            <button
+              type="button"
+              onClick={ () => { copyFunc(`http://localhost:3000/foods/${item.id}`); } }
+            >
+              <img
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ ShareIcon }
+                alt="share-btn"
+              />
+            </button>
+
+            <button
+              type="button"
+              value={ item }
+              onClick={ (e) => favButton(e) }
+            >
+              <img
+                data-testid="favorite-btn"
+                src={ favStatus ? BlackHeartIcon : WhiteHeartIcon }
+                alt="fav-icon"
+              />
+            </button>
+
+            { copied ? <p>Link copied!</p> : undefined}
+
+            { renderTags() }
+          </div>
+
+        </div>
+      );
+    });
+  };
+
+  const foodBtn = () => {
+    const local = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const food = local.filter((a) => a.type === 'food');
+    setAll(food);
+  };
+
+  const drinkBtn = () => {
+    const local = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const drink = local.filter((a) => a.type === 'drink');
+    setAll(drink);
+  };
+
+  return (
+    <>
+      <div>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          onClick={ () => {
+            setAll(JSON.parse(localStorage.getItem('favoriteRecipes')));
+          } }
+        >
+          All
+        </button>
+
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          onClick={ foodBtn }
+        >
+          Food
+        </button>
+
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          onClick={ drinkBtn }
+        >
+          Drinks
+        </button>
+      </div>
+
+      <main>
+        { all ? (
+          renderLocalDone()
+        ) : <p>Loading</p> }
+      </main>
+    </>
+  );
+}
+
+export default ShowFavoriteRecipes;
--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import ShowFavoriteRecipes from '../components/ShowFavoriteRecipes';
 
 function FavoriteRecipes() {
   return (
-    <Header showIcon={ false } titleHeader="Favorite Recipes" />
+    <>
+      <Header showIcon={ false } titleHeader="Favorite Recipes" />
+
+      <ShowFavoriteRecipes />
+    </>
   );
 }
 
